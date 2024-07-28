00ca758a08bcd720ed772329ca0ae98c9598d8ad
Rajnish bhargav <rajbhargav077@gmail.com>
2024-07-28 21:21:56 +0530

    #

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
           transition={{ duration: 1.5 }}
-          href="/public/RajnishSDEresume (1).pdf"
+          href="/RajnishSDEresume (1).pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="my-4 flex items-center justify-center text-white hover:text-purple-300"
